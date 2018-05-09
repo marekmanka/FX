cf9c44fd744ce9628f736de442a740d96f45e10f
marekmanka <marek.manka@poczta.onet.pl>
2018-05-09 10:16:13 +0200

    FX-00002

--- src/pl/mkm/m001/Main.java
+++ src/pl/mkm/m001/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("M001");
+        primaryStage.setTitle("Calculator");
         primaryStage.setScene(new Scene(root, 400, 250));
         primaryStage.show();
         primaryStage.setResizable(false);
