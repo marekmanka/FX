914fe18e249578977a80c6a4b83d589312c93c91
marekmanka <marek.manka@poczta.onet.pl>
2018-05-10 01:07:58 +0200

    FX-0003

--- src/pl/mkm/login/Controller.java
+++ src/pl/mkm/login/Controller.java
@@ -1,19 +1,24 @@
 package pl.mkm.login;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     @FXML
     private Button signIn;
     @FXML
     private Label signInPassed;
 
-    public void initialize(){
-        signInPassed.setVisible(false);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //signInPassed.setVisible(false);
         signInPassed.setText("acb");
         signIn.setOnAction( event -> signInPassed.setText("acb"));
+        System.out.println("sssssss");
     }
-
 }
